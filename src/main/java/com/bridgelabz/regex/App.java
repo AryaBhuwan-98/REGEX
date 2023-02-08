325f3fbfbb630cc8fc5a3c08d3ff3f5f4578b3aa
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-02-08 08:56:52 -0800

    Add:Welcome to the Java Regex

---
+++ src/main/java/com/bridgelabz/regex/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.regex;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome TO THE Java Regex Program" );
+    }
+}
